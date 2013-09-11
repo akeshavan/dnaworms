610bd4dec5ae8ab443cf4564ac45d1c3f27de79c
Anisha Keshavan <akeshavan@ucla.edu>
2013-09-11 15:11:27 -0700

    fix: some whitespace issues

--- pattern.py
+++ pattern.py
@@ -17,12 +17,12 @@ def calculate_shift():
     all_shifts = []
     for i,vs in enumerate(vstrands):
         stap = get_strand_idx(vs["stap"])
-	scaf = get_strand_idx(vs["scaf"])
-	first_stap, last_stap = stap[0], stap[-1]
-	first_scaf, last_scaf = scaf[0], scaf[-1]
-	#find the closest multiple of 21 past staple length 
-	staple_length = last_stap - first_stap
-	if staple_length % 21:
+        scaf = get_strand_idx(vs["scaf"])
+        first_stap, last_stap = stap[0], stap[-1]
+        first_scaf, last_scaf = scaf[0], scaf[-1]
+        #find the closest multiple of 21 past staple length 
+        staple_length = last_stap - first_stap
+        if staple_length % 21:
             shift = staple_length + (21 - staple_length % 21)
         else:
             shift = staple_length
@@ -51,27 +51,27 @@ def do_shift(shift,N=1):
     # remember to check we aren't going to shift past the scaffold strand
     for i,vs in enumerate(vstrands):
         stapidx = get_strand_idx(vs["stap"])
-	scafidx = get_strand_idx(vs["scaf"])
-	for n in range(1,N+1):
-		for stidx in stapidx:
-			if stidx+shift*n > scafidx[-1]:
-					raise Exception("Shift value is too high!")
-			vs["stap"][stidx+shift*n] = add_except_n1(vs["stap"][stidx],shift*n)
+        scafidx = get_strand_idx(vs["scaf"])
+        for n in range(1,N+1):
+            for stidx in stapidx:
+                if stidx+shift*n > scafidx[-1]:
+                    raise Exception("Shift value is too high!")
+                vs["stap"][stidx+shift*n] = add_except_n1(vs["stap"][stidx],shift*n)
 
-    vs["stap_colors"] = vs["stap_colors"]*(N+1) 
+        vs["stap_colors"] = vs["stap_colors"]*(N+1) 
 
 
-if __name__ == "__name__":
-	help = "USAGE: python <filename> <number of repeats> <out filename>"
-	if len(sys.argv) != 4:
-		raise Exception(help)
-	else:
-        f = load_json(sys.argv[1])
-		vstrands = f["vstrands"]
-		num_v = len(vstrands)
-		s = calculate_shift()
-		print "shifting by ", s
-		do_shift(s,sys.argv[2])
-		save_json(sys.argv[3],f)
+help = "USAGE: python <filename> <number of repeats> <out filename>"
+if len(sys.argv) != 4:
+	raise Exception(help)
+else:
+	print sys.argv
+	f = load_json(sys.argv[1])
+	vstrands = f["vstrands"]
+	num_v = len(vstrands)
+	s = calculate_shift()
+	print "shifting by ", s
+	do_shift(s,int(sys.argv[2]))
+	save_json(sys.argv[3],f)
 
 
